648229d9266edb5d77801972b65f011037699544
vms <alifsaidur69@gmail.com>
2026-04-26 09:38:40 +0600

    Update startup.js

--- container/startup.js
+++ container/startup.js
@@ -7,11 +7,11 @@ function startup() {
         console.log('📦 Step 1: Installing PM2...');
         execSync('npm install -g pm2', { stdio: 'inherit' });
 
-        console.log('📦 Step 2: Installing dependencies for container/web/...');
-        execSync('npm install --prefix container/web/', { stdio: 'inherit' });
+        console.log('📦 Step 2: Installing dependencies inside ./web/...');
+        execSync('npm install', { stdio: 'inherit', cwd: './web/' });
 
-        console.log('🚀 Step 3: Starting container/web/index.js as "web"...');
-        execSync('pm2 start container/web/index.js --name web', { stdio: 'inherit' });
+        console.log('🚀 Step 3: Starting index.js using PM2 inside ./web/...');
+        execSync('pm2 start index.js --name web', { stdio: 'inherit', cwd: './web/' });
 
         console.log('💾 Step 4: Saving PM2 process state...');
         execSync('pm2 save', { stdio: 'inherit' });
