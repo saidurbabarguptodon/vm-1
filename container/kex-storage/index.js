7de9c0f991d571deb356a5f063bd3da95354d658
vms <alifsaidur69@gmail.com>
2026-06-06 15:34:58 +0600

    Refactor home route and server start message

--- container/kex-storage/index.js
+++ container/kex-storage/index.js
@@ -4,24 +4,15 @@ const cookieParser = require('cookie-parser');
 const { router: authRoutes, checkSession } = require('./auth');
 
 const app = express();
-
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Authentication Routes
 app.use('/auth', authRoutes);
 
-// Protected Home Route
 app.get('/', checkSession, (req, res) => {
-    // Current tab (overview or settings)
     const view = req.query.view || 'overview';
-    
-    res.render('home', { 
-        user: req.user, 
-        view: view 
-    });
+    res.render('home', { user: req.user, view: view });
 });
 
-const PORT = 8000;
-app.listen(PORT, () => console.log(`Aeronotics running at http://localhost:${PORT}`));
+app.listen(8000, () => console.log('Aeronotics live: http://localhost:8000'));
